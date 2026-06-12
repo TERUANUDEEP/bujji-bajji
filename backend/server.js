2ce92278107aa63784131c3b708f5df49fe958b9
Teru Anudeep <teruanudeep987@gmail.com>
2026-06-12 16:33:37 +0530

    brevo port 2525

--- backend/server.js
+++ backend/server.js
@@ -183,20 +183,20 @@ console.log("BREVO_PASS exists =", !!process.env.BREVO_PASS);
 
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
-  port: 587,
+  port: 2525,
   secure: false,
   auth: {
     user: process.env.BREVO_USER,
     pass: process.env.BREVO_PASS
   }
 });
-transporter.verify((error, success) => {
-  if (error) {
-    console.log("SMTP ERROR:", error);
-  } else {
-    console.log("SMTP READY");
-  }
-});
+// transporter.verify((error, success) => {
+//   if (error) {
+//     console.log("SMTP ERROR:", error);
+//   } else {
+//     console.log("SMTP READY");
+//   }
+// });
 let otpStore = {};
 
 /* =========================
